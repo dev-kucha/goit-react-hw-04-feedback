be71b3675d5da5265f400af8d97a5f5bacaf6eaa
Yurii <ukucher@gmail.com>
2022-08-12 14:47:23 +0300

    bugfix deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react';
+import Section from './Section/Section';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
-import Section from './Section/Section';
 
 export class App extends Component {
   state = {
